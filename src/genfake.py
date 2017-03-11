3666f3cf1dafe9d939198c81fae5c377143d6f2e
Shubham Naik <shubham.naik10@gmail.com>
2017-03-11 14:16:02 -0800

    updated build flow

--- src/genfake.py
+++ src/genfake.py
@@ -1,16 +1,25 @@
 import json
+import os
+import inspect
+
+filename = inspect.getframeinfo(inspect.currentframe()).filename
+path = os.path.dirname(os.path.abspath(filename))
 
 data = {}
 data['jobs'] = []
 
 data['jobs'].append({
-    'title': 'Software Engineer',
-    'tags': ['engineering', 'leadership', 'full time'],
+    'title': 'Director of Engineering',
+    'tags': ['engineering', 'leadership', 'High Commitment'],
 })
 data['jobs'].append({
-    'title': 'Chief Shelley',
-    'tags': ['engineering', 'leadership', 'full time'],
+    'title': 'Software Engineer, R&D',
+    'tags': ['engineering', 'Medium Commitment'],
+})
+data['jobs'].append({
+    'title': 'Director of Finance',
+    'tags': ['finance', 'leadership', 'High Commitment'],
 })
 
-with open('./static/jobs.json', 'w') as outfile:
+with open(path + '/static/jobs.json', 'w') as outfile:
     json.dump(data, outfile, indent=4)
