f1c63f6e93bcf933f27f6335367e95e708b1b7fe
Shubham Naik <shubham.naik10@gmail.com>
2017-03-11 11:44:10 -0800

    add script to make fake jobs

--- src/__init__.py
+++ src/__init__.py
@@ -1,9 +1,17 @@
+# do all the work here
+
 from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return render_template('general/index.html')
+def index():
+    return render_template('general/index.html', jobs = 0)
+
+@app.route('/jobs')
+def jobs():
+    return render_template('general/jobs.html', jobs = 1)
+
+
 #hello
 if __name__ == "__main__":
     app.run()
